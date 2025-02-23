46bf458815a532af536bea4411f073d8b14ec1ea
facusteckler86 <facundomsteckler@gmail.com>
2025-02-23 18:07:22 -0300

    modifico el DetalleProductos

--- src/componentes/modulos/DetalleProducto.jsx
+++ src/componentes/modulos/DetalleProducto.jsx
@@ -4,9 +4,9 @@ import data from "../../db";
 function DetalleProductos() {
 
     
-    const {productosId} = useParams();
-    const productos = data.find((prod)=>prod.id == productosId);
-    const {image, price,title,description} = productos;
+    const {ProductosId} = useParams();
+    const Productos = data.find((prod)=>prod.id == ProductosId);
+    const {image, price,title,description} = Productos;
     
     
     return(
@@ -19,7 +19,7 @@ function DetalleProductos() {
             <h2>{title}</h2>
             <h3>{price}</h3>
             <h3>{description}</h3>
-            <Link to="/productos">Volver</Link>
+            <Link to="/Productos">Volver</Link>
 
         </div>
     )
