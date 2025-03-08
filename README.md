6ab69488161fa4795fdbd9ac09dc178a680f7106
facusteckler86 <facundomsteckler@gmail.com>
2025-03-08 16:29:29 -0300

    armo readme

---
+++ README.md
@@ -0,0 +1,24 @@
+# Proyecto Final de React en CoderHouse
+
+![presentacion del proyecto](../react-steckler/src/assets/images/img%20readme.jpg)
+
+En este proyecto que es de una panaderia, es simil al que hice en el curso de Javascript, tambien realizado en Coder.
+
+##Intrucciones para Instalarlo.
+
+'''bash
+
+git clone https://github.com/facusteckler86/NavegaLasRutas-Steckler.git
+
+'''
+
+no hay que olvidarse de instalar los modulos con npm install
+
+### Tecnologias aplicadas
+
+1. Vite
+2. Firebase
+
+
+
+- [Mi perfil](https://www.linkedin.com/in/facundomsteckler/)
--- src/componentes/modulos/mainPage.jsx
+++ src/componentes/modulos/mainPage.jsx
@@ -1,24 +1,11 @@
 import "../../../src/App.css";
-//import { FaHome } from "react-icons/fa";
-//import { IoMdExit } from "react-icons/io";
-//import { IoMdContact } from "react-icons/io";
-//import { MdOutlineProductionQuantityLimits } from "react-icons/md";
-//import { IoIosAlbums } from "react-icons/io";
 import Productos from "./Productos";
-//import { NavLink } from "react-router-dom";
 
 const MainPage = () => {
   return (
-    <div className="superior">
+    <div className="home-Page">
       <h1 className="homeTitle">Panaderia Doña Pepa</h1>
-      <>
-      {/* <div className="navbar-start">  
-        <NavLink to={`/Main`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option' }>Home</NavLink>
-        <NavLink  to={`/Productos`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option' }>Productos</NavLink>
-        <NavLink to={`/category/camperas`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option' }>Contacto</NavLink>
-        <NavLink to={`/`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option' }>Salir</NavLink>
-    </div> */}
-      </>
+
       <>
         <Productos />
       </>
@@ -27,5 +14,3 @@ const MainPage = () => {
 };
 
 export default MainPage;
-
-
