49920c4718dcc89c62325986010afb88b1307f3e
facusteckler86 <facundomsteckler@gmail.com>
2025-03-19 21:25:57 -0300

    armo modulos de cart y agrego una fuente nueva al proyecto

--- src/componentes/modulos/CheckOut/Checkout.jsx
+++ src/componentes/modulos/CheckOut/Checkout.jsx
@@ -1,9 +1,91 @@
-
+import { useContext, useState } from "react";
+import { CartContext } from "../../../context/CartContext";
+import { db } from "../../../firebase";
+import { addDoc, collection, getDocs, query, Timestamp, where, writeBatch } from "@firebase/firestore";
+import CheckoutForm from "./CheckoutForm"; 
+import "./Checkout.css";
 
 export const Checkout = () => {
+  const [loading, setLoading] = useState(false);
+  const [orderId, setOrderId] = useState("");
 
-    const 
-}
+  const { cart, total, clearCart } = useContext(CartContext);
+
+  const CreateOrder = async ({ name, mail }) => {
+    if (!name || !mail) {
+      console.log("Faltan datos del comprador");
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const objOrder = {
+        buyer: {
+          name,
+          mail,
+        },
+        items: cart,
+        total: total,
+        date: Timestamp.fromDate(new Date()),
+      };
+
+      const batch = writeBatch(db);
+      const outOfStock = [];
+      const ids = cart.map((item) => item.id);
+      const productsRef = collection(db, "products");
+      const productsAddedFromFirestore = await getDocs(query(productsRef, where("id", "in", ids)));
+      const { docs } = productsAddedFromFirestore;
+
+      docs.forEach((doc) => {
+        const product = doc.data();
+        const item = cart.find((item) => item.id === doc.id);
+
+        if (!item) return; // Validación para evitar errores si no encuentra el producto.
+
+        if (product.stock >= item.quantity) {
+          batch.update(doc.ref, { stock: product.stock - item.quantity });
+        } else {
+          outOfStock.push({ ...product, id: doc.id });
+        }
+      });
+
+      if (outOfStock.length === 0) {
+        const order = await addDoc(collection(db, "orders"), objOrder);
+        await batch.commit();
+        setOrderId(order.id);
+        clearCart();
+      } else {
+        console.log("Sin stock:", outOfStock);
+      }
+    } catch (error) {
+      console.log("Error al crear la orden:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (loading) {
+    return <h2 className="pantalla-de-carga">Cargando...</h2>;
+  }
+
+  if (orderId) {
+    return (
+      <h2 className="compra-completa">
+        Gracias por comprar en Doña Pepa, el id de tu compra es: <strong>{orderId}</strong>
+      </h2>
+    );
+  }
+
+  return (
+    <div className="checkout-form">
+      <h2>Checkout</h2>
+      <CheckoutForm onConfirm={CreateOrder} />
+    </div>
+  );
+};
+
+
 
 
 
